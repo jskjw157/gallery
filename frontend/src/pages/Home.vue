150a6d44719a5f087ae1a5b2958b0287ebb2fe8f
johny <jskjw157@gmail.com>
2022-12-28 23:39:41 +0900

    [ADD][HOME] 갤러리 카드 추가

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -16,8 +16,8 @@
         <div class="album py-5 bg-light">
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col" v-for="i in 12" :key="i">
-                        <Card/>
+                    <div class="col" v-for="(item, idx) in state.items" :key="idx">
+                        <Card :item="item" />
                     </div>
                 </div>
             </div>
@@ -27,10 +27,22 @@
 
 <script>
 import Card from '@/components/Card.vue'
+import axios from "axios"
+import { reactive } from 'vue'
 export default {
     name: "Home",
     components: { 
         Card 
+    },
+    setup(){
+        const state = reactive({
+            items: []
+        })
+
+        axios.get("/api/items").then( ({data}) => {
+            state.items = data;
+        })
+        return {state}
     }
 }
 </script>

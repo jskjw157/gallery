a6668cb11ad15e9e938950c1f33ae0df7fca8e93
johny <jskjw157@gmail.com>
2023-01-12 23:28:19 +0900

    [FIX][백엔드] 패키지명 수정, 그래들 JPA dependencies 추가

--- backend/src/main/java/org/johnny/gallery/backend/BackendApplication.java
+++ backend/src/main/java/org/johnny/gallery/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package org.johnny.gallery.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/org/johnny/gallery/backend/itemController.java
+++ backend/src/main/java/org/johnny/gallery/backend/itemController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package org.johnny.gallery.backend;
 
 import java.util.ArrayList;
 import java.util.List;

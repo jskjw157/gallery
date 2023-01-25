ff99ed78c480260d2ab0ffb185aa9239bce4459c
johny <jskjw157@gmail.com>
2023-01-25 23:27:25 +0900

    [ADD][LOGIN] 세션스토리지 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,12 +7,20 @@
 <script>
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
+import store from './scripts/store';
 
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
+  },
+  setup() {
+    const id = sessionStorage.getItem("id");
+
+    if(id){
+      store.commit("setAccount", id);
+    }
   }
 }
 </script>
